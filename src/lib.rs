b2ac497bb6830723335d73c93a105757a1a61f57
Salman Pathan <pathansalman555@gmail.com>
2025-05-03 08:50:30 +0530

    Basic building blocks (#3)
    * add blockheater trait
    
    * implement BlockheaderT trait
    
    * add block trait
    
    * implment BlockT trait

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+use std::fmt::Debug;
+
+pub trait HasherT: Sync + Send {
+    type Output: AsRef<[u8]> + Send + Sync + Clone + Debug + Copy;
+    fn hash(s: &[u8]) -> Self::Output;
+}
+
+pub trait BlockHeaderT: Clone + Send + Sync + Debug + 'static {
+    // Header number
+    type Number: Into<u64>;
+    // Header hash type
+    type Hash: AsRef<[u8]>;
+    // Hashing algorithm;
+    type Hashing: HasherT<Output = Self::Hash>;
+
+    // Creates new header
+    fn new(number: Self::Number, state_root: Self::Hash, parent_hash: Self::Hash) -> Self;
+
+    // Return reference to the header number.
+    fn number(&self) -> &Self::Number;
+
+    // Returns a reference to the state root.
+    fn state_root(&self) -> &Self::Hash;
+
+    // Returns a reference to the parent hash.
+    fn parent_hash(&self) -> &Self::Hash;
+
+    fn encode(&self) -> &[u8] {
+        todo!()
+    }
+    // Returns the hash of the header.
+    fn hash(&self) -> Self::Hash {
+        Self::Hashing::hash(self.encode())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct BlockHeader<Number: Copy + Into<u64>, Hash: HasherT> {
+    pub parent_hash: Hash::Output,
+    pub number: Number,
+    pub state_root: Hash::Output,
+}
+
+impl<Number, Hash> BlockHeaderT for BlockHeader<Number, Hash>
+where
+    Number: Copy + Into<u64> + Send + Sync + Debug + Clone + 'static,
+    Hash: HasherT + Debug + Clone + 'static,
+{
+    type Number = Number;
+    type Hash = Hash::Output;
+    type Hashing = Hash;
+
+    fn new(number: Number, state_root: Hash::Output, parent_hash: Hash::Output) -> Self {
+        Self {
+            number,
+            state_root,
+            parent_hash,
+        }
+    }
+
+    fn number(&self) -> &Self::Number {
+        &self.number
+    }
+
+    fn state_root(&self) -> &Self::Hash {
+        &self.state_root
+    }
+
+    fn parent_hash(&self) -> &Self::Hash {
+        &self.parent_hash
+    }
+
+    fn encode(&self) -> &[u8] {
+        todo!()
+    }
+}
+
+pub trait TransactionT: Clone + Send + Sync + Debug + 'static {}
+
+pub trait BlockT: Clone + Send + Sync + Debug + 'static {
+    type Transaction: TransactionT;
+    type BlockHeader: BlockHeaderT;
+
+    fn header(&self) -> &Self::BlockHeader;
+    fn transactions(&self) -> &[Self::Transaction];
+    fn new(header: Self::BlockHeader, transactions: Vec<Self::Transaction>) -> Self;
+}
+
+#[derive(Clone, Debug)]
+pub struct Block<BlockHeader, Transaction> {
+    // The block header.
+    pub header: BlockHeader,
+    // Transactions in block
+    pub transactions: Vec<Transaction>,
+}
+
+impl<BlockHeader, Transaction> BlockT for Block<BlockHeader, Transaction>
+where
+    BlockHeader: BlockHeaderT,
+    Transaction: TransactionT,
+{
+    type BlockHeader = BlockHeader;
+    type Transaction = Transaction;
+
+    fn header(&self) -> &Self::BlockHeader {
+        &self.header
+    }
+
+    fn transactions(&self) -> &[Self::Transaction] {
+        &self.transactions[..]
+    }
+
+    fn new(header: Self::BlockHeader, transactions: Vec<Self::Transaction>) -> Self {
+        Block {
+            header,
+            transactions,
+        }
+    }
+}
