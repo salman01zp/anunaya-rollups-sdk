45fdd6d007f44df2057f3ce61ca3ad997cb5866f
salman01zp <pathansalman555@gmail.com>
2025-05-10 15:47:45 +0530

    implementation of reed solomon erasure code

--- erasure-code/src/error.rs
+++
@@ -1,7 +0,0 @@
-
-#[derive(Debug, thiserror::Error)]
-pub enum ErasureCodeError {
-        /// Generic error.
-        #[error("{}", _0)]
-        Generic(&'static str),
-}
--- erasure-code/src/traits.rs
+++ erasure-code/src/traits.rs
@@ -1,20 +1,19 @@
-use serde::{Serialize, de::DeserializeOwned};
-use std::fmt::Debug;
-use crate::error::ErasureCodeError;
+use crate::errors::ErasureCodeError;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_std::{fmt::Debug, vec::Vec};
 
 /// Erasure code trait
 /// `T` is the input data type
 pub trait ErasureCode<T> {
     /// Type for each data shares (usually depends on `T`)
-    /// Why so many trait bounds on `Share`? <https://github.com/rust-lang/rust/issues/20671>
     type Share: Debug
         + Clone
         + Eq
         + PartialEq
         + Sync
         + Send
-        + Serialize
-        + DeserializeOwned;
+        + CanonicalSerialize
+        + CanonicalDeserialize;
 
     /// Encode `data` into `data.len() + parity_size` shares.
     fn encode(data: &[T], parity_size: usize) -> Result<Vec<Self::Share>, ErasureCodeError>;
@@ -22,5 +21,4 @@ pub trait ErasureCode<T> {
     /// Decode `shares` into `data_size` data elements.
     /// Return `Result::Err` if `shares.len() < data_size`.
     fn decode(shares: &[Self::Share], data_size: usize) -> Result<Vec<T>, ErasureCodeError>;
-
 }
