799e24ace26de965037ec75748ab69957290d82e
salman01zp <pathansalman555@gmail.com>
2025-05-12 17:01:37 +0530

    define state transition traits

--- core/src/traits/state_machine.rs
+++ core/src/traits/state_machine.rs
@@ -1,3 +1,28 @@
-pub trait DataAvailablityLayer {}
 
-pub trait StateTransitionFunction<D: DataAvailablityLayer> {}
+
+use std::{error::Error, fmt::Debug};
+use super::{BlockHeaderT, BlockT};
+
+pub trait AppState {
+        /// Returns the current state root (32-byte hash)
+        fn state_root(&self) -> [u8; 32];
+        
+        /// Returns the previous state root (for rollback/verification)
+        fn previous_state_root(&self) -> Option<[u8; 32]>;
+    }
+
+
+pub trait StateTransitionFunction {   
+
+        type State: AppState;
+        type Error: Error + Debug + Send + Sync;
+        type BlockHeader: BlockHeaderT;
+        type Block: BlockT;
+
+        // Validate block
+        fn validate_block(state: &Self::State, block: &Self::Block) -> Result<(), Self::Error>;
+
+        // Apply block
+        fn apply_block(state: &mut Self::State, block: &Self::Block) -> Result<(), Self::Error>;
+    
+}    
